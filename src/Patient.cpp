af73a1902bb78dff9a3fc26d2427bce430f1a6ef
Khushi-bit24 <khushiiii3411@gmail.com>
2025-11-21 06:43:49 -0800

    Update Patient.cpp
    PATIENT UPDATION

---
+++ src/Patient.cpp
@@ -0,0 +1,13 @@
+#include "../include/Patient.h"
+
+Patient::Patient(string id, string loc, string urg) {
+    patientID = id;
+    location  = loc;
+    urgency   = urg;
+}
+
+void Patient::requestAmbulance() {
+    cout << "Patient " << patientID 
+         << " requested an ambulance at location " << location
+         << " with urgency: " << urgency << endl;
+}
